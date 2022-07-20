9d9b77e99042b86e600db83d6e432e22b3ca0f71
@khareh <ngongucynthia@gmail.com>
2022-07-20 16:56:48 +0300

    stuff

---
+++ shop/static/js/cart.js
@@ -0,0 +1,14 @@
+$('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
+var updatebtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i < updatebtns.length; i++){
+    updatebtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+    })
+}
